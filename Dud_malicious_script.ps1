f821ef1da29b828347d536c94b4b8a3ab219363c
Bikram Dhindsa <dhindsa.bikram@gmail.com>
2026-07-28 15:09:24 -0700

    Modify script output message
    Added dud poweshell script to called in stage 1 - EXECUTION.

---
+++ Dud_malicious_script.ps1
@@ -0,0 +1,3 @@
+Write-Host "==========================================" -ForegroundColor Cyan
+Write-Host "!!!!!PAylOAd Successfully DEployED !!!!!!!" -ForegroundColor Green
+Write-Host "==========================================" -ForegroundColor Cyan
